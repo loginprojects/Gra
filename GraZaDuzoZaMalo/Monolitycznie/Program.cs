c36c10d0e66a3b0c756badd919dc1916586af00c
loginprojects <55107758+loginprojects@users.noreply.github.com>
2019-09-11 20:25:22 +0200

    Wersja 1

---
+++ GraZaDuzoZaMalo/Monolitycznie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Komputer losuje liczbę z  podanego zakresu
+            Random los = new Random();
+            int wylosowana = los.Next(1, 101);
+#if DEBUG
+            Console.WriteLine(wylosowana); // do usunięcia w Release
+#endif           
+            // powtarzaj wielokrotnie, aż odgadnie
+            // 2. Człowiek proponuje (odgaduje)
+            // 3. Komputer ocenia poropozycję
+            // koniec powtarzania
+
+            // 4. Wypisz statystyki gry
+        }
+    }
+}

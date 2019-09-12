bde3319e7a2ae2e6cc6d554f33075fb2fb83067f
loginprojects <55107758+loginprojects@users.noreply.github.com>
2019-09-12 19:44:00 +0200

    Wersja 4
    Po wycięciu Main1 (archiwalna część kodu)

--- GraZaDuzoZaMalo/Proceduralnie/Program.cs
+++ GraZaDuzoZaMalo/Proceduralnie/Program.cs
@@ -7,7 +7,7 @@ using System.Diagnostics;
 
 namespace Proceduralnie
 {
-    
+
 
     class Program
     {
@@ -29,43 +29,17 @@ namespace Proceduralnie
             return wylosowana;
         }
 
-        static void Main(string[] args)
+        static int WczytajPropozycję()
         {
-            Start()
-        }
-
-
-        static void Main1(string[] args)
-        {
-            // 1. Komputer losuje liczbę z  podanego zakresu
-            Random los = new Random();
-            int wylosowana = los.Next(1, 101);
-#if DEBUG
-            Console.WriteLine(wylosowana); // do usunięcia w Release
-#endif           
-            Console.WriteLine("Wylosowałem liczbę z zakresu od 1 do 100. Odgadnij ją.");
-
-            Stopwatch czas = Stopwatch.StartNew();
-
-            // powtarzaj wielokrotnie, aż odgadnie
-            bool odgadniete = false;        // zmienna nazywana 'flagą' lub  'wartownikiem'; przy zastosowaniu słowa kluczowego 'break' deklarowanie i wykorzytanie zmiennej 'wartownika' jest niepotrzebne - mniej eleganckie, ale praktykowane rozwiązanie
-            int licznik = 0;
-            do
+            int propozycja = 0;
+            Console.WriteLine("Podaj swoją propozycję: \n Wpisz 'koniec', aby zakończyć.");
+            string napis = Console.ReadLine();
+            while (true)
             {
-                licznik++;
-                // 2. Człowiek proponuje (odgaduje)
-                Console.WriteLine("Podaj swoją propozycję: \n Wpisz 'koniec', aby zakończyć.");
-                string napis = Console.ReadLine();
-                if (napis == "koniec")
-                {
-                    Console.WriteLine("Szkoda, że mnie opuszczasz.");
-                    return;
-                }
-
-                int propozycja = 0;
                 try                                  // obsługa błędów. Warto sprawdzić w dokumentacji funkcji (w tym przypadku 'int.Parse'), jakie wyjątki mogą wystąpić
                 {
                     propozycja = int.Parse(napis);
+                    break;
                 }
                 catch (FormatException)
                 {
@@ -86,35 +60,42 @@ namespace Proceduralnie
                     Console.WriteLine("Niezidentyfikowany wyjątek. Awaria");
                     Environment.Exit(1);        //gwatłowanie przerywam działanie programu
                 }
-
-                // 3. Komputer ocenia poropozycję
-                if (propozycja < wylosowana)
-                {
-                    Console.WriteLine("Za mało");
-                }
-                else if (propozycja > wylosowana)
-                {
-                    Console.WriteLine("Za dużo");
-                }
-                else
-                {
-                    Console.WriteLine("Trafiłeś!");
-                    //odgadniete = true;
-                    break;
-                }
-
             }
-            //while (!odgadniete); // while ( propozycja != wylosowana )
-            while (true); //kręć się w nieskończoność - aż do 'break'
-            czas.Stop();
-
-
-
-            // koniec powtarzania
-
-            // 4. Wypisz statystyki gry
-            Console.WriteLine($"Liczba ruchów: {licznik}");
-            Console.WriteLine($"Czas gdy: {czas.Elapsed}");
+            return propozycja;
         }
+
+        static bool Ocena(int wylosowana, int propozycja)
+        {
+            if (propozycja < wylosowana)
+            {
+                Console.WriteLine("Za mało");
+            }
+            else if (propozycja > wylosowana)
+            {
+                Console.WriteLine("Za dużo");
+            }
+            else
+            {
+                Console.WriteLine("Trafiłeś!");
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Start();
+            int x = Losuj();
+            do
+            {
+
+                int y = WczytajPropozycję();
+
+                //oceń propozycję
+            } while (true);
+        }
+
+
+        
     }
 }

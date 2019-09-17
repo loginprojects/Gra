4b8ad75a6244509fd0669cc01caee4190a59cb68
loginprojects <55107758+loginprojects@users.noreply.github.com>
2019-09-17 19:30:06 +0200

    Historia

--- GraZaDuzoZaMalo/GraLib/ModelGry.cs
+++ GraZaDuzoZaMalo/GraLib/ModelGry.cs
@@ -6,18 +6,70 @@ using System.Threading.Tasks;
 
 namespace GraLib
 {
-    public class ModelGry
+    public partial class ModelGry
     {
-        private int wylosowana;
+        //pola/fields
+        private readonly int wylosowana;                    //readonly oznacza, że nie można zmienić wartości poza konstruktorem
         public int ZakresOd { get; private set; }
         public int ZakresDo { get; private set; }
+        public enum Stan { Trwa, Poddana, Odgadnieta }      //diagram stanu aplikacji pomaga przy podejmowaniu decyzji funkcjonalnych - np. pokazanie graczowi wylosowanej liczby na etapie, gdy się 'Poddaje'
+        public Stan StanGry { get; private set; }
 
-        public ModelGry(int zakresOd, int zakresDo)
+        public List<Ruch> Historia { get; private set; }
+
+
+        //Konstruktor
+        public ModelGry(int zakresOd = 1, int zakresDo = 100)
         {
-            ZakresOd = zakresOd;
-            ZakresDo = zakresDo;
+            ZakresOd = Math.Min(zakresOd, zakresDo);
+            ZakresDo = Math.Max(zakresOd, zakresDo);
             Random los = new Random();
-            wylosowana = los.Next(ZakresOd, ZakresDo + 1);
+            wylosowana = los.Next(ZakresOd, ZakresDo + 1); //+1, bo losowanie nie zawiera górnej liczby wskazanego zakresu - właściwość funckji Random
+            StanGry = Stan.Trwa;
+            Historia = new List<Ruch>();
         }
+
+        //Metody
+        public enum Odp { ZaMalo = -1, Trafione = 0, ZaDuzo = 1 };
+
+        public Odp Ocena(int propozycja)
+        {
+            if (propozycja < wylosowana)
+            {
+                //Ruch r = new Ruch(propozycja, Odp.ZaMalo); //prawie równoważne z zapisem poniżej
+                //Historia.Add(r);
+                Historia.Add(new Ruch(propozycja, Odp.ZaMalo));
+                return Odp.ZaMalo;
+            }
+            else if (propozycja > wylosowana)
+            {
+                Historia.Add(new Ruch(propozycja, Odp.ZaDuzo));
+                return Odp.ZaDuzo;
+            }
+            else
+            {
+                Historia.Add(new Ruch(propozycja, Odp.Trafione));
+                StanGry = Stan.Odgadnieta;
+                return Odp.Trafione;
+            }
+
+        }
+
+        public void Poddaj()
+        {
+            StanGry = Stan.Poddana;
+        }
+
+        int Wylosowana
+        {
+            get
+            {
+                if (StanGry == Stan.Poddana || StanGry == Stan.Odgadnieta)
+                    return wylosowana;
+                else
+                    throw new NotSupportedException("Nie wolno teraz Ci tej wartości udostępnić");
+            }
+        }
+
     }
 }
